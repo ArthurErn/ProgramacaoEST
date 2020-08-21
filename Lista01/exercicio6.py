dbb24fdcb7b756bd692f16855b0c4681c33c27ff
GustaBahia <gustavocode18@gmail.com>
2020-08-20 23:42:06 -0300

    Exercício 6

---
+++ Lista01/exercicio6.py
@@ -0,0 +1,6 @@
+a = input ("Digite o primeiro numero: ")
+b = input ("Digite o segundo numero: ")
+a  = int   
+b  = int  
+int soma = a + b
+print ( "A soma de" , a , "+" , b , "eh igual a" , soma )
