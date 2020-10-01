ff3ee98d6a9c04e67d4ab7ed5e757666e9a84811
GustaBahia <gustavocode18@gmail.com>
2020-09-30 23:04:09 -0300

    Lista 04

---
+++ Lista04/exercicio1_incompleto.py
@@ -0,0 +1,15 @@
+# Escreva uma função que recebe dois parâmetros e imprime o menor dos dois. 
+# Se eles forem iguais, imprima que eles sao iguais.
+                
+def main():
+    n1 = int(input('Insira um número: '))
+    n2 = int(input('Insira um número: '))
+    
+    if n1 < n2:
+        
+    
+    if n1 == n2:
+        print('==== Os números são iguais ====')
+        
+if __name__ == "__main__":
+    main()
