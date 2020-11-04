0502ca4dd63d3ee6192000a1dc4e0139671d3498
Baianoware <gustavocode18@gmail.com>
2020-11-03 22:59:22 -0300

    Exercicio de Validação de ip
    Exercicio de Validação de ip

--- Lista04/exercicio11_incompleto.py
+++ Lista04/exercicio11_incompleto.py
@@ -1,16 +1,29 @@
-import re
-
-ip_reg = re.compile(r'^d{1,3}.d{1,3}.d{1,3}.d{1,3}$')   
-    
-def main():
-    inp = input('Insira o endereço IP: ')
-    
-    if inp == ip_reg:
-        print('verdade')
+def verificacaoIP(lista):
+    cont = 0
+    blocoInteiro = ''
+    for item in lista:
+        if item < 0 or item > 255:
+            cont += 0
+        else:
+            cont += 1
+        blocoInteiro += str(item)
+        if cont != 4:
+            blocoInteiro += '.'
+    if cont == 4:
+        print("-" * 30)
+        print(f"O IP {blocoInteiro} é válido!")
     else:
-        print('falso')
-        
-if __name__ == '__main__':
-    main()
+        print(f"IP {blocoInteiro} é inválido!")
+
+
+print("-" * 30)
+print(f"{'VERICADOR DE IPv4':^30}")
+print("-" * 30)
+
+lista = []
+for c in range(1, 4 + 1):
+    ip = int(input("%2dº bloco: " % c))
+    lista.append(ip)
+verificacaoIP(lista)
 
     
